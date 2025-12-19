051062b812b38fc681c35f675fc8544f3329c93b
Robin Lightfoot <robin@eelgirl.biz>
2025-12-19 12:59:06 +0000

    fix: remove comments, update messages, lint

--- src/server.py
+++ src/server.py
@@ -1,32 +1,7 @@
 import subprocess
-# import time
 from wsgiref.simple_server import make_server
 
 import falcon
-
-# import psutil
-
-# def wait_for_beet_processes(parent_pid: int, timeout: int = 3600) -> None:
-#     start_time = time.time()
-#
-#     while time.time() - start_time < timeout:
-#         beet_processes = []
-#         for proc in psutil.process_iter(["pid", "name", "cmdline"]):
-#             try:
-#                 cmdline = proc.info.get("cmdline")
-#                 if cmdline and "beet" in " ".join(cmdline):
-#                     beet_processes.append(proc.info["pid"])
-#             except (psutil.NoSuchProcess, psutil.AccessDenied):
-#                 continue
-#
-#         if not beet_processes:
-#             break
-#
-#         time.sleep(0.5)
-#
-#     if time.time() - start_time >= timeout:
-#         raise TimeoutError(f"Beet processes did not complete within {timeout} seconds")
-#
 
 
 class BeetImportResource:
@@ -35,7 +10,7 @@ class BeetImportResource:
         print(f"Received import request for directory: {data['localDirectoryName']}")
         try:
             print(
-                f"beet --config=config/config.yaml import --quiet {data['localDirectoryName']}"
+                f"beet -v --config=config/config.yaml import --quiet {data['localDirectoryName']}"
             )
             result = subprocess.Popen(
                 [
@@ -47,12 +22,9 @@ class BeetImportResource:
                     data["localDirectoryName"],
                 ]
             )
-            # print("Main beet process completed, waiting for background imports...")
-            # wait_for_beet_processes(result.pid)
-            # print("All import processes completed successfully.")
             resp.status = falcon.HTTP_200
             resp.media = {
-                "message": "Imported complete.",
+                "message": "Import triggered.",
                 "output": f"Beet import process triggered for directory: {data['localDirectoryName']}",
             }
         except Exception as e:
@@ -70,5 +42,5 @@ if __name__ == "__main__":
     with make_server("", 8074, app) as httpd:
         print("Serving on port 8074...")
 
-        # Serve until process is killed
+        # mood
         httpd.serve_forever()
